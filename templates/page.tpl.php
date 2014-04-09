d2c3648b8731785136b14930818d289bec59030d
Anthony K. @Big Mac <off.koss@gmail.com>
2014-04-09 23:18:39 +0400

    Initial insert Twitter button

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -149,6 +149,8 @@
         <?php print render($page['content']); ?>
         <div class="fb-like" data-href="http://pavel-nemzorov.ru" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
         <div class="g-plusone" data-size="medium"></div>
+        <a href="https://twitter.com/share" class="twitter-share-button" data-lang="ru" data-related="PavelNemzorov">Твитнуть</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         <div id="vk_like"></div>
         <script type="text/javascript">
         VK.Widgets.Like("vk_like", {type: "button", height: 18});
